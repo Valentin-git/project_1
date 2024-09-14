1ffad1145cacb3f3f4f223431f024faa3c1ab46b
Valentun <Valentin_Zhdanovsky@proton.me>
2024-09-14 22:07:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,231 @@
+"use strict";
+
+// a = 15;
+// console.log(a)
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// leftBorderWidth = 17;
+// console.log(leftBorderWidth);
+
+// {
+//    let result = 50;
+// }
+
+// console.log(result);
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// console.log("Ширена кузова автомобиля: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+
+// ['vruru', 'ecsdf', 'sfdsdf'].map(a => )
+
+// Request
+// data 
+// response
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+// const COLOR_RED = '#f00';
+
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('tring' * 9);
+
+// const persone = "Alex";
+// const persone = `Alex`;
+// const persone = `5`;
+// const bool = true;
+
+// console.log(sonething);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//    name: "John",
+//    age: 25,
+//    isMarried: false
+// };
+
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+
+
+// const arr = [1, 2, 3];
+// const arr = ['a', 'b', 'c'];
+
+// arr[10] = '3256';
+// console.log(arr[10]);
+// console.log(arr);
+
+// const arrObj = {
+//    a: 'a',
+//    1: 'b',
+//    2: 'c',
+//    abc: {
+//       df: [{}, {}],
+//       def: {
+
+//       }
+//    }
+// };
+
+
+// const b = 'b';
+
+// arrObj[b] = [];
+
+// arrObj.b = '1234';
+// arrObj['b'] = '1234';
+
+// console.log(arr[1]);
+// console.log(arrObj.a);
+// console.log(arrObj[1]);
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+// const obj = {a: 1, b: 2};
+// const obj = {
+//    Anna: 500,
+//    'Alice': 800,
+// };
+
+
+// alert('Hello');
+
+// const result = confirm("Are you here");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+// console.log(typeof(null));
+
+
+// const category = 'toys';
+
+// console.log('https://someurl.com/' + category);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Hello, ${user}`);
+
+
+// console.log('arr' + " - object");
+// console.log(4 + +" 5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+// ++incr;
+// --decr;
+
+// console.log(incr++);
+// console.log(decr--);
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 === 8);
+console.log(2 + 4 * 2 === 8);
+console.log(2 + 2 * 2 != 8);
+
+const isChecked = false,
+      isClose = false;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+// console.log(isChecked || !isClose);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
